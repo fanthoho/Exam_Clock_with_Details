100ffff360d4622b474c6cf59510c239f0edd89b
fanthoho <fantho@gmail.com>
2025-03-03 08:47:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
         const now = new Date();
         const startTime = new Date(now.getTime());
         const endTime = new Date(now.getTime() + minutes * 60000)
-        const hstr1 = startTime.getHours();
-        const mstr1 = startTime.getMinutes();
-        const startTimeString  = hstr1 + ":" + mstr1;
-        
+        const startTimeString  = startTime.getHours() + ":" + startTime.getMinutes();
+        const endTimeString  = endTime.getHours() + ":" + endTime.getMinutes();
+                
         selectedSubjectDisplay.textContent = subjectSelect.options[subjectSelect.selectedIndex].text;
         minutesDisplay.textContent = `${startTimeString} - ${endTimeString} (total ${minutes} minutes)`;
         timeLeft = minutes * 60;
